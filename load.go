a85c78ba14ae7527850b1ad31bfb62bab114334e
Aotokitsuruya <contact@aotoki.me>
2023-09-07 20:20:45 +0800

    Move newProc to proc.go

--- load.go
+++ load.go
@@ -31,51 +31,6 @@ func (s *State) Load(r io.Reader) (Value, error) {
 	return proc.Execute(s)
 }
 
-func newProc(r io.Reader) (*proc, error) {
-	var header binaryHeader
-	err := binaryRead(r, &header)
-	if err != nil {
-		return nil, err
-	}
-
-	var executable *irep
-
-	remain := header.Size - binaryHeaderSize
-	for remain > sectionHeaderSize {
-		var header sectionHeader
-		err := binaryRead(r, &header)
-		if err != nil {
-			return nil, err
-		}
-
-		isOverSize := header.Size > remain
-		if isOverSize {
-			return nil, ErrSectionOverSize
-		}
-
-		switch header.String() {
-		case sectionTypeIrep:
-			executable, err = readIrep(r, header.Size)
-		case sectionTypeDebug:
-			err = noopSection(r, header.Size)
-		case sectionTypeLv:
-			err = noopSection(r, header.Size)
-		case sectionTypeEof:
-			break
-		}
-
-		if err != nil {
-			return nil, err
-		}
-
-		remain -= header.Size
-	}
-
-	return &proc{
-		executable: executable,
-	}, nil
-}
-
 func readIrep(r io.Reader, size uint32) (*irep, error) {
 	var riteVersion [4]byte
 	err := binaryRead(r, &riteVersion)
--- proc.go
+++ proc.go
@@ -1,9 +1,56 @@
 package mruby
 
+import "io"
+
 type executable interface {
 	Execute(state *State) (Value, error)
 }
 
 type proc struct {
 	executable
+}
+
+func newProc(r io.Reader) (*proc, error) {
+	var header binaryHeader
+	err := binaryRead(r, &header)
+	if err != nil {
+		return nil, err
+	}
+
+	var executable *irep
+
+	remain := header.Size - binaryHeaderSize
+	for remain > sectionHeaderSize {
+		var header sectionHeader
+		err := binaryRead(r, &header)
+		if err != nil {
+			return nil, err
+		}
+
+		isOverSize := header.Size > remain
+		if isOverSize {
+			return nil, ErrSectionOverSize
+		}
+
+		switch header.String() {
+		case sectionTypeIrep:
+			executable, err = readIrep(r, header.Size)
+		case sectionTypeDebug:
+			err = noopSection(r, header.Size)
+		case sectionTypeLv:
+			err = noopSection(r, header.Size)
+		case sectionTypeEof:
+			break
+		}
+
+		if err != nil {
+			return nil, err
+		}
+
+		remain -= header.Size
+	}
+
+	return &proc{
+		executable: executable,
+	}, nil
 }
